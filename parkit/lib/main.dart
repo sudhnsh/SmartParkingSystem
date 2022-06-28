ed85122a220a1424ccf644b87db5e09f5bc5dcd5
Sudhanshu <sudhnsh16@gmail.com>
2022-06-28 18:59:27 +0530

    NavBar Added

---
+++ parkit/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import './navbar.dart';
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(drawer: NavBar(),);
+
+        
+    
+  }
+}
